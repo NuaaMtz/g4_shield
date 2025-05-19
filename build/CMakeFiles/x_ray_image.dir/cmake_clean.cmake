dfb1b10b8779ae02c1b5bb123df7f53a7f0e8f02
mtz <nuaamzt@nuaa.edu.cn>
2025-05-19 22:44:34 +0800

    新建文件夹

---
+++ build/CMakeFiles/x_ray_image.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/x_ray_image"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/x_ray_image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cc
@@ -0,0 +1,14 @@
+/*
+ * @Author: mtz nuaamzt@nuaa.edu.cn
+ * @Date: 2025-05-19 22:40:29
+ * @LastEditors: mtz nuaamzt@nuaa.edu.cn
+ * @LastEditTime: 2025-05-19 22:41:45
+ * @FilePath: /g4_shield/main.cc
+ * @Description: 主函数
+ */
+ #include "iostream"
+ #include "G4MTRunManager.hh"
+int main(){
+std::cout << "Hello World!" << std::endl;
+G4MTRunManager *runManager = new G4MTRunManager();
+}
